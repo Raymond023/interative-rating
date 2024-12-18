be78f4c9dc5156291355af45d0264c6073baba8d
Raymond023 <raymondraphael11@gmail.com>
2024-12-18 12:20:05 +0100

    modified HTML for improved semantics and accessibilitty

--- script.js
+++ script.js
@@ -3,11 +3,23 @@
 const btn = $('.button');
 const ratingSection = $('.rating-section');
 const message = $('.message');
-const submitBtn = $('.submit');
+const submitBtn = $('.submit-btn');
 const userRating =$('.user-rating');
 
 submitBtn.on('click', handleSubmitEvent);
 btn.on('click', handleButtonEvent);
+
+submitBtn.on('keydown', function(e) {
+    if (e.key === 'Enter') {
+        handleSubmitEvent.call(this)
+    }
+});
+
+btn.on('keydown', function(e) {
+    if (e.key === 'Enter') {
+        handleButtonEvent.call(this)
+    }
+});
 
 let selectedButton;
 
@@ -15,8 +27,7 @@ function handleSubmitEvent () {
     if(selectedButton) {
         ratingSection.css('display', 'none');
         message.css('display', 'block');
-
-        userRating.text(selectedButton)
+        userRating.text(selectedButton);
     }
 }
 
