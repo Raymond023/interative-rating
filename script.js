7a3e3a50a2f3e60e6839686bae091aa055008501
Raymond023 <raymondraphael11@gmail.com>
2025-01-02 12:57:54 +0100

    changed the HTML to be semantically correct and improve accessibility, removed redundant click event and used css for checked radio, removed jQuery dependence

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
 'use strict';
 
-const btn = $('.button');
-const ratingSection = $('.rating-section');
-const message = $('.message');
-const submitBtn = $('.submit-btn');
-const userRating =$('.user-rating');
+const ratingSection = document.querySelector('.rating-section');
+const message = document.querySelector('.message');
+const form = document.querySelector('form');
+const input = document.querySelectorAll('input')
+const userRating = document.querySelector('.user-rating');
 
-submitBtn.on('click', handleSubmitEvent);
-btn.on('click', handleButtonEvent);
-
-submitBtn.on('keydown', function(e) {
-    if (e.key === 'Enter') {
-        handleSubmitEvent.call(this)
-    }
-});
-
-btn.on('keydown', function(e) {
-    if (e.key === 'Enter') {
-        handleButtonEvent.call(this)
-    }
-});
-
-let selectedButton;
-
-function handleSubmitEvent () {
-    if(selectedButton) {
-        ratingSection.css('display', 'none');
-        message.css('display', 'block');
-        userRating.text(selectedButton);
-    }
-}
-
-function handleButtonEvent () {
-    btn.removeClass('selected');
-    $(this).addClass('selected');
-    selectedButton = $(this).attr('id');
-}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userRating.textContent = [...input].find(radio => radio.checked).value;
+    ratingSection.style.display = 'none';
+    message.style.display = 'block';
+})
